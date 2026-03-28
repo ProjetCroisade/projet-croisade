700ba6790a274e63b86b8ff0bb83723a3300ac20
Fropaccino <bouffardjulien84@gmail.com>
2026-03-28 18:35:59 -0400

    Update visibilité du site

--- data/js/search.js
+++ data/js/search.js
@@ -2,3 +2,10 @@ new PagefindUI({
   element: "#search",
   showSubResults: true
 });
+
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('nav ul li a').forEach(link => {
+  if (link.getAttribute('href') === currentPage) {
+    link.classList.add('active');
+  }
+});
