29803a2b14901e76ad90c7633ba6fff5bca2423c
Fropaccino <bouffardjulien84@gmail.com>
2026-04-17 21:14:18 -0400

    Update menus header

--- data/js/search.js
+++ data/js/search.js
@@ -9,3 +9,28 @@ document.querySelectorAll('nav ul li a').forEach(link => {
     link.classList.add('active');
   }
 });
+
+// Dropdown navigation
+document.querySelectorAll('.has-dropdown').forEach(item => {
+  let closeTimer = null;
+
+  item.addEventListener('mouseenter', () => {
+    clearTimeout(closeTimer);
+    item.classList.add('open');
+  });
+
+  item.addEventListener('mouseleave', () => {
+    closeTimer = setTimeout(() => {
+      item.classList.remove('open');
+    }, 150);
+  });
+});
+
+// Close dropdown on outside click
+document.addEventListener('click', e => {
+  if (!e.target.closest('.has-dropdown')) {
+    document.querySelectorAll('.has-dropdown.open').forEach(item => {
+      item.classList.remove('open');
+    });
+  }
+});
